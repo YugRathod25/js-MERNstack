a02641bf93621ee876481f1b85e0f64ad1114dc1
Yug Rathod <yugrathod1025@gmail.com>
2026-05-29 03:53:32 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3+3)  // we are using node js, not browser
+
+let name = "Yug"
+let age = 19
+let isLoggedIn = true
+
+// different data types
+
+// number => basically int 
+// big int => big numbers that are out of the range of numbers
+// string => " "
+// boolean => true/false
+// null => standalone value
+//undefined => its basically a not defined value
+// symbol =>  used for uniqueness, mainly used in react
+// objects
+
+// typeof function:
+
+console.log(typeof "yug")
+console.log(typeof age)
+console.log(typeof null) // o/p: object
+console.log(typeof undefined) // o/p: undefined
