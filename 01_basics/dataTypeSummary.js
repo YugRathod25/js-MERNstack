31e9562fdf71ce37ab2fdcd361f6524d12ba52d7
Yug Rathod <yugrathod1025@gmail.com>
2026-05-31 04:21:08 +0000

    datatype summary

---
+++ 01_basics/dataTypeSummary.js
@@ -0,0 +1,32 @@
+// Primitive 
+
+// 7 types : String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+// Reference (Non-Primitive)
+
+// Array, Objects, Functions 
+
+// array
+const heros = ["ironman", "spidy", "hulk"];
+
+// obj
+
+const myObj = {
+    name = "yug",
+    age = 19
+}
+
+// function
+
+const myFunc = function()
+{
+    console.log("Hello World");
+    
+}
+
+// typeof function is used to find which type of datatype the variable is 
+
+
+
+//JavaScript is a dynamically typed language. Because data type will automatically assigned at the time of compilation or code execution.
+
