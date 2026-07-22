d26bba121df4210981245418e050dca9ee0519ca
Yug Rathod <yugrathod1025@gmail.com>
2026-07-22 13:56:26 +0000

    start of prototype

---
+++ 13_oops/prototype.js
@@ -0,0 +1,19 @@
+// with prototype we can create our own function as we have map function for array 
+
+function createUser(username, price){
+    this.username = username
+    this.price = price
+}
+
+createUser.prototype.increment = function(){
+    this.price++
+}
+
+createUser.prototype.printMe = function(){
+    console.log(`The price is: ${this.price}`);   
+}
+
+const chai = new createUser("chai", 20)
+
+chai.increment()
+chai.printMe()
